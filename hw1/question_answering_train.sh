5c4f3056e7b268295ff78a88ccde0ce49bf8825e
Brian-Konr <r12922050@ntu.edu.tw>
2023-10-08 14:51:57 +0800

    chore: script update

--- hw1/question_answering_train.sh
+++ hw1/question_answering_train.sh
@@ -1,10 +1,13 @@
+# export CUDA_VISIBLE_DEVICES=1 # NVIDIA GeForce
+export CUDA_VISIBLE_DEVICES=0 # NVIDIA RTX A5000
+
 python question_answering.py \
     --train_file="./data/train.json" \
     --validation_file="./data/valid.json" \
     --context_file="./data/context.json" \
     --model_name_or_path="bert-base-chinese" \
     --max_seq_length=512 \
-    --per_device_train_batch_size=1 \
+    --per_device_train_batch_size=16 \
     --gradient_accumulation_steps=2 \
     --num_train_epochs=1 \
     --learning_rate=3e-5 \
