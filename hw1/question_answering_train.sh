9286a6ae175e1d0d0e82ad7d4f851ef84708ff18
Brian-Konr <r12922050@ntu.edu.tw>
2023-10-14 21:43:15 +0800

    use roberta and add more epochs

--- hw1/question_answering_train.sh
+++ hw1/question_answering_train.sh
@@ -1,15 +1,15 @@
 # export CUDA_VISIBLE_DEVICES=1 # NVIDIA GeForce
 # export CUDA_VISIBLE_DEVICES=0 # NVIDIA RTX A5000
-export CUDA_VISIBLE_DEVICES=3 # Tesla K80 (GPU 1)
+export CUDA_VISIBLE_DEVICES=0 # Tesla K80 (GPU 1)
 
 python question_answering.py \
     --train_file="./data/train.json" \
     --validation_file="./data/valid.json" \
     --context_file="./data/context.json" \
-    --model_name_or_path="bert-base-chinese" \
+    --model_name_or_path="hfl/chinese-roberta-wwm-ext" \
     --max_seq_length=512 \
-    --per_device_train_batch_size=1 \
+    --per_device_train_batch_size=8 \
     --gradient_accumulation_steps=2 \
-    --num_train_epochs=1 \
+    --num_train_epochs=3 \
     --learning_rate=3e-5 \
     --output_dir="./qa-ckpt"
